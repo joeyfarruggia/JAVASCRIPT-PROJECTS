ad48e42f29c8d2fa315ef5667da35107e309c90e
joeyfarruggia <joeyfarruggia66@gmail.com>
2026-03-11 12:08:54 -0700

    Following JS Course
    Adding and updating files following along the JS course

---
+++ Basic-Javascript-Projects/Project1-expressions-alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Check this out!");
+
+var A = "\"Man, I hope there isn't another pop-up\" - someone about to get another pop-up";
+
+window.alert(A);
+
+var B = "Oops! Another pop-up!";
+
+window.alert(B);
