70f72ce82b52b371797a44e6a028eadd22080787
joeyfarruggia <joeyfarruggia66@gmail.com>
2026-03-12 10:36:10 -0700

    Update main.js

--- Basic-Javascript-Projects/Project1-expressions-alert/JS/main.js
+++ Basic-Javascript-Projects/Project1-expressions-alert/JS/main.js
@@ -1,19 +1,23 @@
 window.alert("Check this out!");
 
-var A = "\"Man, I hope there isn't another pop-up\" - someone about to get another pop-up";
+var A = "\"Man, I hope there isn't another pop-up\" - someone about to get another pop-up"; //A backslash is used to add quotation marks inside of a string.
 
-window.alert(A);
+window.alert(A); //window.alert() is a pop-up that will show the variable that is called in it.
 
 var B = "Oops! Another pop-up!";
 
 window.alert(B);
 
 document.write("This is the end of the pop-ups," 
-    + " I promise...");
+    + " I promise..."); //The + symbol is used to concatenate, or combine, two strings together.
 
 var C = " But wait," + " there's more!";
 
 document.write(C);
 
-var Family = "The Farruggia's"; Dad = " Joey"; Mom = " Sarah"; Son1 = " Ryder"; Son2 = " Andrew"; Daughter = " Olivia";
+var Family = "The Farruggia's"; Dad = " Joey"; Mom = " Sarah"; Son1 = " Ryder"; Son2 = " Andrew"; Daughter = " Olivia"; 
+//This variable is showing that you can have multiple variables in one line, and can then select a specific one from the list.
+
 document.write(Dad);
+
+document.write(6 * 6); //This is showing a math expression, which JavaScript can perform the calculation and show the answer.
