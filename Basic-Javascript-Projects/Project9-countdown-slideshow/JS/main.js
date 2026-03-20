9734e276b0f99033685453210d4a12af877ffd09
joeyfarruggia <joeyfarruggia66@gmail.com>
2026-03-19 17:35:38 -0700

    Project 9 Completion

---
+++ Basic-Javascript-Projects/Project9-countdown-slideshow/JS/main.js
@@ -0,0 +1,47 @@
+//This countdown function will create a timer counting down 
+// from the selected number to 0 and then say times up.
+
+function countdown() {
+    var seconds = document.getElementById("seconds").value
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's Up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Next and Previous Controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
