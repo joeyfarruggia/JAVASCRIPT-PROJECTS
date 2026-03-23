c210a64e5a73c0987c71f730d900e9d9543427e0
joeyfarruggia <joeyfarruggia66@gmail.com>
2026-03-23 15:34:47 -0700

    JS Project Updates

---
+++ Basic-Javascript-Projects/Project10-loop-arrays/JS/main.js
@@ -0,0 +1,19 @@
+function Call_Loop() {
+var Digit = "";
+var X = 1;
+while (X <= 20) {
+    Digit += "<br>" + X;
+    X++;
+}
+document.getElementById("Loop").innerHTML = Digit;
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List-of-Instruments").innerHTML = Content;
+}
